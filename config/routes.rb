4ba1d073a0cd3b19928f64d4d7716963d350a73f
yasuhiko suzuki <horizuninbestment@yahoo.co.jp>
2017-08-24 15:21:37 +0000

    Finish layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SecondApp::Application.routes.draw do
-  get "top_pages/home"
+  root  'top_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
